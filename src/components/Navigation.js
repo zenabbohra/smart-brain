3d9de9fd8974fa415c35610cea631df781b348df
Zenab <zenab.bohra19@gmail.com>
2020-01-09 16:16:31 +1100

    Added Navigation bar with Sign out link

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navigation = () => {
+  return(
+    <div>
+      {/*<nav style={{display:'flex', justifyContent:'flex-end'}}>*/}
+      <nav className='tr'>
+        <p className='underline pa3 f3 dim pointer link'>Sign Out</p>
+      </nav>
+    </div>
+  );
+}
+
+export default Navigation;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import 'tachyons';
+import Navigation from '../components/Navigation';
 
 function App() {
   return (
     <div>
-
+      <Navigation/>
+      {/*<Logo/>*/}
+      {/*<ImageUrlInput/>*/}
+      {/*<FaceRecognition/>*/}
     </div>
   );
 }
