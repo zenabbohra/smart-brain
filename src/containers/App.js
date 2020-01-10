b622f2cba6ac955427d41497181d814ebc361863
Zenab <zenab.bohra19@gmail.com>
2020-01-10 12:31:00 +1100

    Added particles background using react-particles.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,26 @@ import Navigation from '../components/Navigation';
 import Logo from '../components/Logo/Logo';
 import ImageUrlInput from '../components/ImageUrlInput';
 import FaceRecognition from '../components/FaceRecognition';
+import Particles from 'react-particles-js';
+
+const particleOptions = {
+  "particles": {
+    "number": {
+      "value": 100
+    },
+    "size": {
+      "value": 3
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      }
+    }
+  }
+};
 
 class App extends Component {
   constructor(props) {
@@ -24,9 +44,10 @@ class App extends Component {
   };
 
   render() {
-    const { imageUrl, input } = this.state;
+    const { imageUrl } = this.state;
     return (
       <div>
+        <Particles className='particles' params={particleOptions}/>
         <Navigation/>
         <Logo/>
         <ImageUrlInput onInputChange={this.onInputChange} onButtonClick={this.onButtonClick}/>
