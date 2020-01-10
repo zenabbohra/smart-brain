572f5d2f2bd54349be71188dfd2df20489605640
Zenab <zenab.bohra19@gmail.com>
2020-01-10 11:56:27 +1100

    Added Image input field and detect button

--- src/components/ImageUrlInput.js
+++ src/components/ImageUrlInput.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const ImageUrlInput = () => {
   return(
-    <div>
-
+    <div className='tc'>
+      <p className='f4 black-70'>This Magic Brain will detect faces in your pictures. Git it a try. </p>
+      <input
+        className='pa1 w-30-l shadow-5 bw1 b--white'
+        type='text'
+        placeholder='Insert face image url here'
+      />
+      <button className="f6 link dim ph3 pv2 mb2 dib white bg-light-purple"
+      >Detect</button>
     </div>
   );
 };
