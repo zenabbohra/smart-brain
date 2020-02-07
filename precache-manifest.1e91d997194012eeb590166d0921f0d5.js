b853a087909e2b20430510027e892d257704e3ba
Zenab <zenab.bohra19@gmail.com>
2020-02-07 16:31:29 +1100

    Updates

---
+++ precache-manifest.1e91d997194012eeb590166d0921f0d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db108c1e6dd8db8e2f769a2b6cf09ef",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "6708925d6db6b98a8464",
+    "url": "/smart-brain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "fbd665b258ad61dc69ac",
+    "url": "/smart-brain/static/css/main.bf909399.chunk.css"
+  },
+  {
+    "revision": "6708925d6db6b98a8464",
+    "url": "/smart-brain/static/js/2.e4bf9303.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/smart-brain/static/js/2.e4bf9303.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fbd665b258ad61dc69ac",
+    "url": "/smart-brain/static/js/main.5bbd3b8d.chunk.js"
+  },
+  {
+    "revision": "2beedbd042bf4b799852",
+    "url": "/smart-brain/static/js/runtime-main.f39d4b8b.js"
+  }
+]);
